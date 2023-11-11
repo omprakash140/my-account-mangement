e62081fbcec2141b67d5f30e5e756a7662c6e645
Om Gupta <om.gupta@teknopoint.in>
2023-11-11 21:06:14 +0530

    1. Refetch GET Queries after delete client

--- client/src/component/ClientRow.jsx
+++ client/src/component/ClientRow.jsx
@@ -1,10 +1,12 @@
 import { FaTrash } from 'react-icons/fa';
 import { useMutation } from '@apollo/client';
 import { DELETE_CLIENT } from '../mutation/clientMutation';
+import { GET_CLIENTS } from '../queries/clientQuery';
 
 export default function ClientRow({ client }) {
     const [deleteClient] = useMutation(DELETE_CLIENT, {
-        variables: { id: client.id }
+        variables: { id: client.id },
+        refetchQueries : [ { query : GET_CLIENTS}]
     });
     return (
         <tr>
