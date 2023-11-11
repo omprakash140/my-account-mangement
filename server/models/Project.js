b98b6048ff365e6da88baa3097a4b96b96691b0d
Om Gupta <om.gupta@teknopoint.in>
2023-11-11 15:01:06 +0530

    1. CRUD Operation added for Project 2. Done with Backend Code

--- server/models/Project.js
+++ server/models/Project.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const ProjectSchema = new mongoose.Schema({
     name: { type: String },
     description: { type: String },
-    status: { type: String, enum: ['Not Started', 'In Progess', 'Completed'] },
+    status: { type: String, enum: ['Not Started', 'In Progress', 'Completed'] },
     clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'Client' }
 
 })
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,4 +1,4 @@
-const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLList, GraphQLBoolean, GraphQLNonNull } = require('graphql');
+const { GraphQLObjectType, GraphQLString, GraphQLID, GraphQLSchema, GraphQLList, GraphQLBoolean, GraphQLNonNull, GraphQLEnumType } = require('graphql');
 // const { clients, projects } = require('../sampleData');
 const Client = require('./../models/Client');
 const Project = require('./../models/Project');
@@ -11,7 +11,7 @@ const ProjectType = new GraphQLObjectType({
         name: { type: GraphQLString },
         description: { type: GraphQLString },
         status: { type: GraphQLString },
-        client: { type: ClientType, resolve(parent, args) { return Client.findById(parent.id) } },
+        client: { type: ClientType, resolve(parent, args) { return Client.findById(parent.clientId) } },
     })
 })
 
@@ -74,6 +74,75 @@ const mutation = new GraphQLObjectType({
             args: { id: { type: GraphQLNonNull(GraphQLID) } },
             resolve(parent, args) {
                 return Client.findByIdAndDelete(args.id)
+            }
+        },
+        addProject: {
+            type: ProjectType,
+            args: {
+                name: { type: GraphQLNonNull(GraphQLID) },
+                description: { type: GraphQLNonNull(GraphQLID) },
+                status: {
+                    type: new GraphQLEnumType({
+                        name: "ProjectStatus",
+                        values: {
+                            new: { value: "Not Started" },
+                            progress: { value: "In Progress" },
+                            completed: { value: "Completed" }
+                        }
+                    }),
+                    defaultValue: "Not Started",
+                },
+                clientId: {
+                    type: GraphQLNonNull(GraphQLID)
+                }
+            },
+            resolve(parent, args) {
+                const project = new Project({
+                    name: args.name,
+                    description: args.description,
+                    status: args.status,
+                    clientId: args.clientId,
+                })
+                return project.save();
+            }
+        },
+        deleteProject: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) }
+            },
+            resolve(parent, args) {
+                return Project.findByIdAndDelete(args.id)
+            }
+        },
+        updateProject: {
+            type: ProjectType,
+            args: {
+                id: { type: GraphQLNonNull(GraphQLID) },
+                name: { type: GraphQLString },
+                description: { type: GraphQLString },
+                status: {
+                    type: new GraphQLEnumType({
+                        name: "ProjectStatusUpdate",
+                        values: {
+                            new: { value: "Not Started" },
+                            progress: { value: "In Progress" },
+                            completed: { value: "Completed" }
+                        }
+                    }),
+                }
+            },
+            resolve(_, args) {
+                return Project.findByIdAndUpdate(args.id,
+                    {
+                        $set: {
+                            name: args.name,
+                            description: args.description,
+                            status: args.status,
+                        }
+                    },
+                    { new: true }
+                );
             }
         }
     }
