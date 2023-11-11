2ae1263666b3efc0a4ba2dd3732389c29895ddbc
Om Gupta <om.gupta@teknopoint.in>
2023-11-12 01:20:29 +0530

    1. Render Data from cache after mutation

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,23 @@ import Client from "./component/Client";
 import Header from "./component/Header";
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming
+          }
+        }
+      }
+    }
+  }
+})
 const client = new ApolloClient({
   uri: "http://localhost:8000/graphql",
-  cache: new InMemoryCache()
+  cache
 })
 
 
